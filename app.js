c0898948c835279287bfb9246c9d224e6d60c786
clara <clara@clara.com>
2016-02-16 14:13:37 -0600

    simple page counter for multiple pages

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
 var express = require('express');   //Need express...
 var logger = require('morgan');   //Enable logging
-//var cookieParser = require('cookie-parser'); //handle cookies
-var session = require('express-session')  //no longer need cookie-parser.
+var session = require('express-session')  //To handle sessions
+var parseurl = require('parseurl') //get the actual url requested, will use to count visits to particular pages.
 
 app = express();         //Create app
-
 app.use(logger('dev'));   //Initialize logger - do this first
 
+app.use(session({
+  secret: "random_number_here"  //replace in production app
+}));
 
- app.use(session({
-   secret: "av3ua3wvu9a3y89a3cqy8n9ac3wny8m"
-  }));
+//All routes will run this function first/
+//Calling next() at the end of this function will run the next matching route handler
+app.use(function(req, res, next){
 
-//Create a route to the home page
-app.get('/', function(req, res){
-  var session = req.session;
-  if (session.views) {
-    session.views++
-    res.send("You've been to this page " + session.views + " times ");
-  } else {
-    session.views = 1
-    res.send("Welcome, first time visitor")
+  var views = req.session.views
+  if (!views) {
+    views = req.session.views = {}; //first time visitor - set up view counter
   }
+
+  var path = parseurl(req).pathname   //get the URL used in this request
+
+  if (!req.session.views[path]) {    //If no record for this URL yet...
+    req.session.views[path] = 1;
+  } else {                           //User has already visited this page, add 1 to page count
+    req.session.views[path]++ ;
+  }
+
+  next();    //And call the next matching route handler
+
+});
+
+
+app.get('/', function(req, res, next){
+  res.send("home page")
 })
 
 app.get('/cat', function(req, res){
   res.send('Cat page')
 });
 
-app.get('/bird', function(req, res){
-  console.log(req.cookies)
-  res.send('Bird page');
-});
-
 app.get('/fish', function(req, res) {
   res.send('Fish page');
 });
 
+
+app.get('/counts', function(req, res){
+  res.send("You have visited these pages, " + JSON.stringify(req.session.views))
+});
 
 app.listen(3040);  //And listen on port 3040.
